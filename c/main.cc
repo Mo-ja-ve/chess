f58be6f51bbac7f66e0275db3369db10557fa8c7
0xBAMA <jon.baker1222@gmail.com>
2019-08-12 16:04:48 -0400

    compiles

--- c/main.cc
+++ c/main.cc
@@ -8,15 +8,16 @@ typedef struct b_node
 
 //can now declare a board_node
 
-int int main(int argc, char const *argv[]) {
+int main(int argc, char const *argv[]) {
 
   board_node current_space;
 
   current_space.x = 5;
   current_space.y = 3;
 
-  cout << "the current space is " << current_space.x << " " << current_space.y;
-  cout << endl;
+  std::cout << "the current space is ";
+  std::cout << current_space.x << " ";
+  std::cout << current_space.y << std::endl;
 
   return 0;
 }
