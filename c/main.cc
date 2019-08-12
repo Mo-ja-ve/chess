378d960fe3b63a87c7111228cd42cd6ffa7c2ea9
0xBAMA <jon.baker1222@gmail.com>
2019-08-12 16:09:48 -0400

    as a pointer

--- c/main.cc
+++ c/main.cc
@@ -10,14 +10,16 @@ typedef struct b_node
 
 int main(int argc, char const *argv[]) {
 
-  board_node current_space;
+  // board_node current_space;
 
-  current_space.x = 5;
-  current_space.y = 3;
+  // current_space.x = 5;
+  // current_space.y = 3;
+
+  board_node * A0_ptr = new board_node();
 
   std::cout << "the current space is ";
-  std::cout << current_space.x << " ";
-  std::cout << current_space.y << std::endl;
+  std::cout << A0_ptr->x << " ";
+  std::cout << A0_ptr->y << std::endl;
 
   return 0;
 }
